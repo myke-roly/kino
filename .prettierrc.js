b127cdc8437c023fd3a64740aebfd79af2d4be91
mike <mike@underscope.io>
2021-01-15 12:08:17 -0300

    configure prettier

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 120,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  singleQuote: true,
+  semi: false,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: 'es5',
+}
